7c93151aa66626d2116ed3ba189286fef4644d8e
Eric Abrahamsen <eric@ploonky.com>
2015-11-08 18:13:57 -0500

    Use album images.

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -1,7 +1,8 @@
 class GalleryController < ApplicationController
   before_action :authenticate_user!, except: [:index] 
   def index
-  	@images = Image.all
+  	@album = Album.first
+  	@images = Image.where(imageable_id: @album.id, imageable_type: 'Album')
   	@full_images = @images.map{|a| { href: a.photo.url, title: 'test'} }
   end
 end
