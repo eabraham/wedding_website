ff4867177c984bec6032c4f1fae81c3206c5912f
Eric Abrahamsen <eric@ploonky.com>
2016-01-17 17:08:32 -0500

    Be explicit about assets.

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ Bundler.require(*Rails.groups)
 module Ericandasmita
   class Application < Rails::Application
     config.autoload_paths << Rails.root.join('lib')
+
+    config.assets.precompile += %w( .svg .eot .woff .ttf .png .jpg .gif .jpeg)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
