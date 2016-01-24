0063f24e8eed1d03f609d29cce2914e4d96a3ccd
Eric Abrahamsen <eric@ploonky.com>
2016-01-23 21:40:42 -0500

    Add travel page. And car icon.

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,7 @@ class Event < ActiveRecord::Base
 
   accepts_nested_attributes_for :images, allow_destroy: true
 
-  VALID_EVENT_ICONS = ['picture', 'location', 'movie', 'plane', 'diamond', 'heart', 'menu', 'home']
+  VALID_EVENT_ICONS = ['picture', 'location', 'movie', 'plane', 'diamond', 'heart', 'menu', 'home', 'car']
 
   validate :date_start, :presence
   validate :date_end, :presence
