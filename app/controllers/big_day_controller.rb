8050bee9e61c344354152f909874109f1be077f0
Eric Abrahamsen <eric@ploonky.com>
2016-02-29 20:52:12 -0500

    Place wedding details behind authentication.

--- app/controllers/big_day_controller.rb
+++ app/controllers/big_day_controller.rb
@@ -1,5 +1,5 @@
 class BigDayController < ApplicationController
-  before_action :authenticate_user!, except: [:index] 
+  before_action :authenticate_user!
   def index
   end
 end
--- app/controllers/travel_controller.rb
+++ app/controllers/travel_controller.rb
@@ -1,5 +1,5 @@
 class TravelController < ApplicationController
-  before_action :authenticate_user!, except: [:index] 
+  before_action :authenticate_user! 
   def index
   end
 end
